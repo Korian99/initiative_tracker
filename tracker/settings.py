8a755ef184353cb94fcfc052428e53ddbc62e446
@misrailev <misrailev@cajapopular.gov.ar>
2025-03-10 09:53:05 -0300

    trying out aiven

--- tracker/settings.py
+++ tracker/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'players',
-    # "channels",
 ]
 
 # CHANNEL_LAYERS = {
@@ -77,15 +76,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION  = 'tracker.wsgi.application'
 
+import dj_database_url
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
