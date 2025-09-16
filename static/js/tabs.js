9331cf9b17128642710ff6d538ba2b6effc6313c
@misrailev <misrailev@cajapopular.gov.ar>
2025-09-16 13:59:51 -0300

    change buttons to floating

--- static/js/tabs.js
+++ static/js/tabs.js
@@ -3,3 +3,26 @@ function activateTab(tabId) {
   var tab = new bootstrap.Tab(tabElement);
   tab.show();
 }
+
+document.getElementById('switch-tab-btn').addEventListener('click', function() {
+    // Find whichever tab nav is visible
+    let activeTab = document.querySelector('.nav-link.active');
+
+    if (!activeTab) return; // no tab active, bail
+
+    let targetId = activeTab.getAttribute('id');
+
+    // Logic for Characters/Add Characters
+    if (targetId === 'characters-tab') {
+        new bootstrap.Tab(document.getElementById('add-characters-tab')).show();
+    } else if (targetId === 'add-characters-tab') {
+        new bootstrap.Tab(document.getElementById('characters-tab')).show();
+    }
+
+    // Logic for Lobbies
+    else if (targetId === 'my-lobbies-tab') {
+        new bootstrap.Tab(document.getElementById('other-lobbies-tab')).show();
+    } else if (targetId === 'other-lobbies-tab') {
+        new bootstrap.Tab(document.getElementById('my-lobbies-tab')).show();
+    }
+});
