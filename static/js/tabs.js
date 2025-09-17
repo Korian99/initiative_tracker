70b4a0a9b76476202961bcb9d07043213f217dd0
@misrailev <misrailev@cajapopular.gov.ar>
2025-09-17 08:53:28 -0300

    changed some static positions

--- static/js/tabs.js
+++ static/js/tabs.js
@@ -4,25 +4,30 @@ function activateTab(tabId) {
   tab.show();
 }
 
-document.getElementById('switch-tab-btn').addEventListener('click', function() {
-    // Find whichever tab nav is visible
-    let activeTab = document.querySelector('.nav-link.active');
-
-    if (!activeTab) return; // no tab active, bail
-
-    let targetId = activeTab.getAttribute('id');
-
-    // Logic for Characters/Add Characters
-    if (targetId === 'characters-tab') {
-        new bootstrap.Tab(document.getElementById('add-characters-tab')).show();
-    } else if (targetId === 'add-characters-tab') {
-        new bootstrap.Tab(document.getElementById('characters-tab')).show();
-    }
-
-    // Logic for Lobbies
-    else if (targetId === 'my-lobbies-tab') {
-        new bootstrap.Tab(document.getElementById('other-lobbies-tab')).show();
-    } else if (targetId === 'other-lobbies-tab') {
-        new bootstrap.Tab(document.getElementById('my-lobbies-tab')).show();
+document.addEventListener("DOMContentLoaded", () => {
+    const switchBtn = document.getElementById('switch-tab-btn');
+    if (switchBtn){
+        switchBtn.addEventListener('click', function() {
+            // Find whichever tab nav is visible
+            let activeTab = document.querySelector('.nav-link.active');
+            if (!activeTab) return; // no tab active, bail
+        
+            let targetId = activeTab.getAttribute('id');
+        
+            // Logic for Characters/Add Characters
+            if (targetId === 'characters-tab') {
+                new bootstrap.Tab(document.getElementById('add-characters-tab')).show();
+            } else if (targetId === 'add-characters-tab') {
+                new bootstrap.Tab(document.getElementById('characters-tab')).show();
+            }
+        
+            // Logic for Lobbies
+            else if (targetId === 'my-lobbies-tab') {
+                new bootstrap.Tab(document.getElementById('other-lobbies-tab')).show();
+            } else if (targetId === 'other-lobbies-tab') {
+                new bootstrap.Tab(document.getElementById('my-lobbies-tab')).show();
+            }
+        });
     }
 });
+
